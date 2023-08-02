1e8a04bade837b8c42d80f9ea2feb90a7787bd13
ltm88 <87349837+ltm88@users.noreply.github.com>
2023-08-02 17:49:13 +0800

    Update turn_to_eval.py

--- eval/turn_to_eval.py
+++ eval/turn_to_eval.py
@@ -3,6 +3,7 @@ import argparse
 import pandas as pd
 import codecs
 import sys
+import csv
 from conlleval import *
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 parser = argparse.ArgumentParser()
@@ -11,7 +12,7 @@ parser.add_argument("--prediction",default='../Predict/predict.txt',type=str,hel
 args = parser.parse_args()
 args = vars(args)
 #read file
-df_truth =  pd.read_csv(args["truth"],encoding='utf-8',delimiter=' ',header=None,skip_blank_lines =False)
-df_prediction =  pd.read_csv(args["prediction"],encoding='utf-8',delimiter=' ',header=None,skip_blank_lines =False)
+df_truth =  pd.read_csv(args["truth"],encoding='utf-8',delimiter=' ',header=None,skip_blank_lines =False, quoting=csv.QUOTE_NONE)
+df_prediction =  pd.read_csv(args["prediction"],encoding='utf-8',delimiter=' ',header=None,skip_blank_lines =False, quoting=csv.QUOTE_NONE)
 df = pd.concat([df_truth,df_prediction[1]],axis=1)
 df.to_csv("eval.txt",encoding='utf-8',header=None,index=None,sep=" ")
